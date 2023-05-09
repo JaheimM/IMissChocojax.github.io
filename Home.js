a84fe46f9994fe0edb405bdab6fc81f938e613c9
JaheimM <84516545+JaheimM@users.noreply.github.com>
2023-05-09 14:39:25 +0000

    Added auto refreash

--- Home.js
+++ Home.js
@@ -2,6 +2,38 @@ document.addEventListener("DOMContentLoaded", function () {
   const h1 = document.querySelector("h1");
   console.log(window.getComputedStyle(h1).fontSize);
 
+  const fetch = require('node-fetch');
+const channelName = '39daph';
+const isLive = false;
+
+async function main(){
+    let a = await fetch(`https://www.twitch.tv/${channelName}`);
+    if( (await a.text()).includes('isLiveBroadcast') )
+    {
+      console.log(`${channelName} is live`);
+      isLive = true;
+    }
+    else
+    {
+      console.log(`${channelName} is not live`);
+      isLive = false;
+    }
+}
+
+main();
+// Set up auto refreash
+
+function autoRefreash(t) {
+  setTimeOut( () => { body.location.reload()}, t
+  )
+}
+// Make code to update website for when isLive is true
+
+const body = document.getElementsByTagName('body');
+
+body.addEventListener("load", autoRefreash(3000));
+
+
   /*    async function isStreamerLive(username) {
       const theUrl = `https://api.twitch.tv/helix/streams?user_login=${username}`;
       const headers = {
